51de93a41c4ffaf815bbf8f0be7dfe9eb56b6933
Alex <astokolyas@bostonsd.ru>
2024-06-22 12:12:30 +0300

    add new realisation

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-import telebot
-from telebot import types
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, MessageHandler, filters
+from creds import BOT_TOKEN, BOT_USERNAME, WEBAPP_URL
+import json
 
-bot = telebot.TeleBot("6972956528:AAGzrauQ_uZjmkorgPlTr529o8IZyTT0j2o")
-game_short_name='taptap'
 
-@bot.message_handler(commands=['start'])
-def send_game(message):
-    user_id = message.chat.id
-    
-    game_url = f'https://disk.yandex.ru/i/EZgO-mantyqi0A'
-    
-    markup = types.InlineKeyboardMarkup(row_width=2)
+async def launch_web_ui(update: Update, callback: CallbackContext):
+    # display our web-app!
+    kb = [
+        [KeyboardButton(
+            "Show me my Web-App!",
+            web_app=WebAppInfo(WEBAPP_URL)
+        )]
+    ]
+    await update.message.reply_text("Let's do this...", reply_markup=ReplyKeyboardMarkup(kb))
 
-    game_button = types.InlineKeyboardButton("Play Game", callback_game="your_game_data")
-    markup.add(game_button)
 
-    bot.send_game(user_id, game_short_name, reply_markup=None, disable_notification=True, timeout=False)
+async def web_app_data(update: Update, context: CallbackContext):
+    data = json.loads(update.message.web_app_data.data)
+    await update.message.reply_text("Your data was:")
+    for result in data:
+        await update.message.reply_text(f"{result['name']}: {result['value']}")
 
-bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    # when we run the script we want to first create the bot from the token:
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    # and let's set a command listener for /start to trigger our Web UI
+    application.add_handler(CommandHandler('start', launch_web_ui))
+
+    # as well as a web-app listener for the user-inputted data
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
+
+
+    # and send the bot on its way!
+    print(f"Your bot is listening! Navigate to http://t.me/{BOT_USERNAME} to interact with it!")
+    application.run_polling()
