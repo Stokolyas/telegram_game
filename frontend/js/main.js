5b57e09c902f447771c4e31344adb01c45e2f469
Alex <astokolyas@bostonsd.ru>
2024-06-24 20:09:12 +0300

    fix

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,12 +1,14 @@
 import { Game } from './game.js';
 import { UI } from './ui.js';
 
-const game = new Game();
-const ui = new UI(game);
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game();
+    const ui = new UI(game);
 
-window.game = game; // Делаем объект game доступным глобально для тестирования
-window.ui = ui; // Делаем объект ui доступным глобально для тестирования
+    window.game = game; // Делаем объект game доступным глобально для тестирования
+    window.ui = ui; // Делаем объект ui доступным глобально для тестирования
 
-// Инициализация Telegram WebApp
-Telegram.WebApp.ready();
-game.initTelegram();
+    // Инициализация Telegram WebApp
+    Telegram.WebApp.ready();
+    game.initTelegram();
+});
