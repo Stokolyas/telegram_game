a421de3e536671d310121e3b0201e70356ee55bb
Alex <astokolyas@bostonsd.ru>
2024-08-07 00:26:31 +0300

    test

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,9 +1,19 @@
 export class API {
-    checkSubscription(telegramId, taskId) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(true); // Mocking subscription check
-            }, 1000);
+    saveGame(gameData) {
+        return fetch(`http://localhost:5000/save`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(gameData),
         });
+    }
+
+    async loadGame(telegramId) {
+        const response = await fetch(`http://localhost:5000/load/${telegramId}`);
+        if (!response.ok) {
+            throw new Error('Ошибка загрузки игры');
+        }
+        return response.json();
     }
 }
