96efa5bbb76385ce3e31a206cfb8180b2e4ebd0a
Alex <astokolyas@bostonsd.ru>
2024-08-06 20:23:18 +0300

    experiment

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,11 +1,8 @@
 export class API {
     checkSubscription(telegramId, taskId) {
-        // Пример вызова API для проверки подписки
         return new Promise((resolve, reject) => {
-            // Замените на ваш реальный API-запрос
             setTimeout(() => {
-                // Мокаем проверку подписки
-                resolve(true); // Предполагаем, что пользователь подписан
+                resolve(true); // Mocking subscription check
             }, 1000);
         });
     }
